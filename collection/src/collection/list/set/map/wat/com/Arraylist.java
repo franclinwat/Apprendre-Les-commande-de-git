d377015be8185f2870984aa43f803c9216cdc7ac
franclin <franclinwat@yahoo.fr>
2020-03-24 00:56:40 +0200

    modification dans les arraylists

--- collection/src/collection/list/set/map/wat/com/Arraylist.java
+++ collection/src/collection/list/set/map/wat/com/Arraylist.java
@@ -8,13 +8,13 @@ public class Arraylist {
 	
 	    ArrayList al = new ArrayList();
 	    al.add(12);
-	    al.add("Une chaîne de caractères !");
-	    al.add(12.20f);
+	    al.add("Une chaîne de plusieur charactère !");
+	    al.add(12.9000f);
 	    al.add('d');
 	                
 	    for(int i = 0; i < al.size(); i++)
 	    {
-	      System.out.println("donnée à l'indice " + i + " = " + al.get(i));
+	      System.out.println("donné à l'indice " + i + " = " + al.get(i));
 	    }               
 	  }
 	}
